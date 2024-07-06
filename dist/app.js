10db160840891e00cdfb34a6834a3ff0eb4bf30b
Rony <rony@Ronys-Air.lan>
2024-07-06 10:39:01 +0600

    done

--- dist/app.js
+++ dist/app.js
@@ -14,7 +14,7 @@ app.use((0, cors_1.default)({
     origin: [
         'http://localhost:3000',
         'http://localhost:3001',
-        'https://lost-fount-client.vercel.app',
+        'https://prothfolio-nextjs.vercel.app'
     ],
     credentials: true,
 }));
